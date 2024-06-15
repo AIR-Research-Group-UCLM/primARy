ed66f26ed06c2782bb92355641cf9d912a303275
Pablo del Hoyo Abad <pablo.hoyo@alu.uclm.es>
2024-06-15 13:01:15 +0200

    fix: remove unused code

--- llm/app/db.py
+++ llm/app/db.py
@@ -16,38 +16,3 @@ vector_index = VectorStoreIndex.from_vector_store(
     vector_store=vector_store,
     embed_model=embeddings.embedding_model
 )
-
-
-def _setup_qdrant():
-    """This function must be run just one time. Since we are always going to filter
-    by the protocol id, this function adjust the indexes of the main collection
-    so that a global search is not performed, improving the performance
-    """
-
-    qdrant_client.create_collection(
-        collection_name=config.MAIN_COLLECTION_NAME,
-        vectors_config=models.VectorParams(
-            size=embeddings.EMBEDDING_DIMENSION,
-            distance=models.Distance.COSINE
-        ),
-        # TODO: search what these parameters mean
-        hnsw_config=models.HnswConfigDiff(
-            payload_m=16,
-            m=0,
-        ),
-    )
-
-    qdrant_client.create_payload_index(
-        collection_name=config.MAIN_COLLECTION_NAME,
-        field_name="protocol_id",
-        field_type=models.PayloadSchemaType.KEYWORD,
-    )
-
-    qdrant_client.update_collection(
-        collection_name="main",
-        hnsw_config=models.HnswConfigDiff(payload_m=16, m=0),
-    )
-
-
-if __name__ == "__main__":
-    _setup_qdrant()
